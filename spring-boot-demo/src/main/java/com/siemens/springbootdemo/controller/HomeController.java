e017bcb9d7b4ad0d692b9ebf4d23f9fb4318a900
Jet Yang <1239143744@qq.com>
2021-10-18 15:48:25 +0800

    添加keycloak到springboot

---
+++ spring-boot-demo/src/main/java/com/siemens/springbootdemo/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.siemens.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yyj
+ * @date 2021-10-18 14:55
+ */
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/customer")
+    public String customer() {
+        return "only customer can see";
+    }
+
+    @RequestMapping("/admin")
+    public String admin() {
+        return "only admin cas see";
+    }
+}
